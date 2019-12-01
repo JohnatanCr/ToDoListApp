32744760fef79958c6f1633ab48527cb9b252892
Marco <marcochong.27@gmail.com>
2019-11-30 22:07:30 -0600

    Agregada la primera versión del ND(Navigation Drawer), agregados varios resources incluyendo drawables y se agregó una dependencia para el ND.

---
+++ app/src/main/java/com/modelo/todolistapp/PlannedTasksFragment.kt
@@ -0,0 +1,24 @@
+package com.modelo.todolistapp
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PlannedTasksFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_planned_tasks, container, false)
+    }
+
+
+}

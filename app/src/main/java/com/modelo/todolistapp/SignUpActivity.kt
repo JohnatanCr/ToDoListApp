2a175899a3b6f7213ed6750e08616553427ce2c4
JohnatanCr <cuevas.cristianjohnatan@hotmail.com>
2019-11-30 22:55:24 -0600

    Edited Some XML issues and Added to test the Navigation Activity By cleacking "Create Account" on SignUp
    Signed-off-by: JohnatanCr <cuevas.cristianjohnatan@hotmail.com>

--- app/src/main/java/com/modelo/todolistapp/SignUpActivity.kt
+++ app/src/main/java/com/modelo/todolistapp/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.modelo.todolistapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,11 +12,19 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val editText_emailRegister:TextInputEditText = findViewById(R.id.editText_emailRegister)
-        val editText_userNameRegister:TextInputEditText = findViewById(R.id.editText_userNameRegister)
-        val editText_passwordRegister:TextInputEditText = findViewById(R.id.editText_passwordRegister)
-        val editText_confirmPasswordRegister:TextInputEditText = findViewById(R.id.editText_confirmPasswordRegister)
+        val editText_emailRegister: TextInputEditText = findViewById(R.id.editText_emailRegister)
+        val editText_userNameRegister: TextInputEditText =
+            findViewById(R.id.editText_userNameRegister)
+        val editText_passwordRegister: TextInputEditText =
+            findViewById(R.id.editText_passwordRegister)
+        val editText_confirmPasswordRegister: TextInputEditText =
+            findViewById(R.id.editText_confirmPasswordRegister)
         val button_createAccount: Button = findViewById(R.id.button_createAccount)
         val button_toLogin: Button = findViewById(R.id.button_toLogin)
+
+        button_createAccount.setOnClickListener {
+            val intent = Intent(this, NavigationDrawerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

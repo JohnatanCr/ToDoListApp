e248bd8f617a394175f415843c911b24a76707d0
JohnatanCr <cuevas.cristianjohnatan@hotmail.com>
2019-12-04 04:26:02 -0600

    Revert "Editor de tareas con errores, CRISTIAN HELP"
    This reverts commit 3661b314
    
    Signed-off-by: JohnatanCr <cuevas.cristianjohnatan@hotmail.com>

--- app/src/main/java/com/modelo/todolistapp/View/MainActivity.kt
+++ app/src/main/java/com/modelo/todolistapp/View/MainActivity.kt
@@ -1,90 +1,13 @@
 package com.modelo.todolistapp.View
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.database.FirebaseDatabase
 import com.modelo.todolistapp.R
 
-
-class DemoAdapter(private val tareas: ArrayList<Tarea>) : RecyclerView.Adapter<DemoAdapter.DemoViewHolder>()
-{
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DemoViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onBindViewHolder(holder: DemoViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    class DemoViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        private var tvName: TextView
-        private var tvDate: TextView
-        private var checkboxito : CheckBox
-        private var tvExternoName : TextView
-        //private var btnDelete:Button
-
-        init {
-            tvName = view.findViewById(R.id.textname)
-            tvDate = view.findViewById(R.id.idFecha)
-            checkboxito = view.findViewById(R.id.idcheck)
-            tvExternoName = view.findViewById(R.id.idExterno)
-            //btnDelete=view.findViewById(R.id.delete_button)
-        }
-
-        public fun bind(tarea: Tarea) {
-            tvName.setText("${tarea.nombretarea}")
-            tvDate.setText("${tarea.fechavenc}")
-            tvExternoName.setText(("${tarea.nameexterno}"))
-
-
-            checkboxito.setOnClickListener {
-                val database = FirebaseDatabase.getInstance()
-                val usersRef = database.getReference("app").child("Tareas")
-                usersRef.child(tarea.id.toString()).removeValue()
-                Snackbar.make(it, "Tarea BORRADA!", Snackbar.LENGTH_INDEFINITE).setAction("Deshacer",
-                    {
-                        Snackbar.make(it, "Tarea recuperada!", Snackbar.LENGTH_LONG).show()
-                    })
-                    .show() //LOCAL
-            }
-        }
-    }
-}
-}
-
-
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var rv: RecyclerView
-    private var tareas: ArrayList<Tarea> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        rv = findViewById<RecyclerView>(R.id.rv).apply {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = DemoAdapter(tareas)
-        }
-
-
-
     }
 }
